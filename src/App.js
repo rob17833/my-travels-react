13f06b9bc6364261f5c688c7fb02fb7d19ebd680
rob17833 <janssensrobert@hotmail.com>
2019-04-09 19:17:20 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Travel from "./Travel";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1 className="App-title">Travel around the world</h1>
+        
+        
+        <Travel
+          destination="Fjallraven"
+          country="Sueeden"
+          photo="https://images.unsplash.com/photo-1543166803-f0309083d510?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
+        />
+        <Travel
+          destination="Lofoten"
+          country="Norway"
+          photo="https://images.unsplash.com/photo-1543651425-3260f9c9ecfb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
+        />
       </div>
     );
   }
---
+++ src/Travel.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Travel = ({destination, country, photo, distance}) => (
+  <figure>
+    <img src={photo} alt="paysage" />
+    <figcaption>
+      <blockquote>{destination}</blockquote>
+      <cite>{country}</cite>
+      <cite>{distance}</cite>
+    </figcaption>
+  </figure>
+);
+
+export default Travel;
